9f677d7ea9aa9e9c9f0a62de59516c7d27e009db
wangyp <619938467@qq.com>
2018-02-07 19:30:43 +0800

    locad sensitive tree function

---
+++ testproject/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+type wordtree struct {
+	tree map[string]*wordtree
+	isEnd bool
+}
+
+var sensitiveWordMap wordtree
+
+func main() {
+	sensitiveWords := []string{"a","peng","test","go"}
+	for _ , value := range sensitiveWords {
+		nowMap := &sensitiveWordMap
+		wordLenth := len(value)
+		for i := 0 ; i < wordLenth ; i ++ {
+			char := string(value[i])
+			wordMap := nowMap.tree[char]
+			if wordMap != nil {  
+				nowMap = wordMap
+			}else{
+				var newtree wordtree
+				if(nowMap.tree == nil){
+					nowMap.tree = make(map[string]*wordtree)
+				}
+				nowMap.tree[char] = &newtree
+				nowMap = &newtree
+			}
+			if(i == wordLenth-1){
+				nowMap.isEnd = true
+			}
+		}
+	}
+	fmt.Print(sensitiveWordMap.tree["p"].tree["e"].tree["n"].tree["g"])
+}
